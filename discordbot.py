f660b7b99d66adc1af115846a68dc8392f3fdd69
taria-boot <61919188+taria-boot@users.noreply.github.com>
2020-03-22 16:31:21 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -6,11 +6,6 @@ bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 @bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-    
 async def on_message(message):
     global current_ans
     if message.author.bot: # メッセージの送信者がBotなら何もしない
