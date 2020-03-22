52ae6bc47671d260c3820224475d695b7ebfefcc
taria-boot <61919188+taria-boot@users.noreply.github.com>
2020-03-23 00:32:53 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,12 +5,13 @@ import traceback
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
-@bot.event
+@bot.event 
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)       
- 
+    
+@bot.event 
 activity = discord.Activity(name='わん', type=discord.ActivityType.watching)
      await client.change_presence(status=discord.Status.idle, activity=activity)    
     
